82e3241585eb65175203a300cfb0a3b382e33d04
nitin643 <73420636+nitin643@users.noreply.github.com>
2022-10-24 00:04:18 +0530

    Add files via upload

---
+++ Day 2 Operators.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip=meal_cost*(tip_percent/100)
+    tax=meal_cost*(tax_percent/100)
+    total=round(meal_cost+tip+tax)
+    print(total)
+    
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
