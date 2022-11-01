ab8ce4641ddd3ae8d6d914838cd140f420200f00
Nitin Srivastava <73420636+nitin643@users.noreply.github.com>
2022-11-01 23:59:07 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+def is_even(i):
+    if type(i)==int:
+      if i%2==0:
+         return "Even"
+      else:
+         return "Odd"
+    else:
+        return "not allowed"     
+
+
+#for i in range(1,11):
+    #print(is_even(i))
+
